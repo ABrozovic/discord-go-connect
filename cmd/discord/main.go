e0a5a2afd71b4e49f2c5cd7149116914bb537353
ABrozovic <alejandro.brozovic@gmail.com>
2023-09-26 16:53:13 -0400

    feat: add main

---
+++ cmd/discord/main.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"discord-go-connect/internal"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+
+	"github.com/joho/godotenv"
+)
+
+func main() {
+	go func() {
+		http.HandleFunc("/ws", internal.WsEndpoint)
+		log.Println("Starting WebSocket server on localhost:8080")
+		err := http.ListenAndServe(":8080", nil)
+		if err != nil {
+			log.Fatal("Failed to start WebSocket server:", err)
+		}
+	}()
+
+	log.Println("Starting channel listener")
+	go internal.ListenToWsChannel()
+
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatalln("DISCORD_BOT_TOKEN missing")
+	}
+	botToken := os.Getenv("DISCORD_BOT_TOKEN")
+
+	bot := internal.NewBot(botToken)
+
+	err = bot.Start()
+	if err != nil {
+		log.Fatal("Failed to start the bot:", err)
+		return
+	}
+
+	log.Println("Bot is now running. Press Ctrl+C to stop.")
+
+	sc := make(chan os.Signal, 1)
+	signal.Notify(sc, syscall.SIGINT, syscall.SIGTERM, os.Interrupt)
+	<-sc
+
+	err = bot.Stop()
+	if err != nil {
+		log.Println("Failed to stop the bot:", err)
+	}
+
+}
